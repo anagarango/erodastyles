2ccf7afd320f39954f16a786048137f675ec60cd
anagarango <anitag.arango@icloud.com>
2024-10-21 00:04:43 -0700

    main page

---
+++ src/app/components/menu.tsx
@@ -0,0 +1,91 @@
+"use client"
+import Image from "next/image";
+import { useRouter } from 'next/navigation'
+import { DownOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { Space, Dropdown } from 'antd';
+import type { MenuProps } from 'antd';
+
+export default function Menu() {
+    const r = useRouter()
+
+    const about: MenuProps['about'] = [
+        {
+          key: '1',
+          label: (
+            <h1 style={{fontSize:"16px"}} onClick={()=>r.push("/author")}>ABOUT THE AUTHOR</h1>
+          )
+        },
+        {
+          key: '2',
+          label: (
+						<h1 style={{fontSize:"16px"}} onClick={()=>r.push("/author")}>ABOUT THE SINGER</h1>
+          )
+        }
+    ];
+		const funsies: MenuProps['funsies'] = [
+			{
+				key: '1',
+				label: (
+					<h1 style={{fontSize:"16px"}}  onClick={()=>r.push("/author")}>Gallery</h1>
+				)
+			},
+			{
+				key: '2',
+				label: (
+					<h1 style={{fontSize:"16px"}}  onClick={()=>r.push("/author")}>Quiz</h1>
+				)
+			},
+			{
+				key: '3',
+				label: (
+					<h1 style={{fontSize:"16px"}}  onClick={()=>r.push("/author")}>Sources</h1>
+				)
+			},
+			{
+				key: '4',
+				label: (
+					<a style={{fontSize:"16px"}}  target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+						Harry Styles is Overrated 
+					</a>
+				)
+			}
+		];
+		const inspirations: MenuProps['inspirations'] = [
+			{
+				key: '1',
+				label: (
+					<h1 style={{fontSize:"16px"}} onClick={()=>r.push("/author")}>Musical Influences</h1>
+				)
+			},
+			{
+				key: '2',
+				label: (
+					<h1 style={{fontSize:"16px"}} onClick={()=>r.push("/author")}>Recent Projects</h1>
+				)
+			}
+		];
+  return (
+    <div style={{display:"flex", fontSize:"16px" ,fontWeight:"300", justifyContent:"center", gap:"35px", padding:"15px 0"}}>
+      <Dropdown menu={{ items:about }}>
+        <Space>
+          ABOUT
+          <CaretDownOutlined style={{ color: '#F4CCC2' }}/>
+        </Space>
+      </Dropdown>
+      <h1>BLOG</h1>
+      <Dropdown menu={{ items:funsies }}>
+        <Space>
+          FUNSIES
+          <CaretDownOutlined style={{ color: '#F4CCC2' }}/>
+        </Space>
+      </Dropdown>
+      <h1>CONTACT</h1>
+      <Dropdown menu={{ items:inspirations }}>
+        <Space>
+          INSPIRATIONS
+          <CaretDownOutlined style={{ color: '#F4CCC2' }}/>
+        </Space>
+      </Dropdown>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,56 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from 'next/navigation'
+import { DownOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { Space, Dropdown } from 'antd';
+import type { MenuProps } from 'antd';
+import Menu from "./components/menu";
+import 'react-multi-carousel/lib/styles.css';
+import { useRef, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+   
+  }, []);
+  return (
+    <div >
+			<Menu/>
+			<div className="header" style={{display:"flex", flexDirection:"column", alignItems:"center", padding:"60px 0"}}>
+				<h1 id="heading" style={{fontSize:"55px", fontWeight:"600", width:"fit-content"}}>Ultimate Guide to Harry Styles</h1>
+				<div className="behind"></div>
+				<h1>A blog dedicated to Grammy winner, singer, songwriter, and actor Harry Styles</h1>
+			</div>
+			<div id="carousel">
+				<div style={{backgroundImage:"url('/hs-grammys.png')", backgroundPosition:"center", backgroundSize:"cover", position:"relative"}}>
+					<h1 style={{backgroundColor:"white", position:"absolute", padding:"40px", top:"45%", left:"40%"}}>Grammy Winner</h1>
+				</div>
+				<div style={{backgroundImage:"url('/hs-singer.png')", backgroundPosition:"center", backgroundSize:"cover", position:"relative"}}>
+					<h1 style={{backgroundColor:"white", position:"absolute", padding:"40px", top:"45%", left:"40%"}}>Grammy Winner</h1>
+				</div>
+				<div style={{backgroundImage:"url('/hs-actor.png')", backgroundPosition:"center", backgroundSize:"cover", position:"relative"}}>
+					<h1 style={{backgroundColor:"white", position:"absolute", padding:"40px", top:"45%", left:"40%"}}>Grammy Winner</h1>
+				</div>
+				<div style={{backgroundImage:"url('/hs-model.png')", backgroundPosition:"center", backgroundSize:"cover", position:"relative"}}>
+					<h1 style={{backgroundColor:"white", position:"absolute", padding:"40px", top:"45%", left:"40%"}}>Grammy Winner</h1>
+				</div>
+			</div>
+
+			<div style={{padding:"70px"}}>
+				<div style={{backgroundColor:"#FCF2F1", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"3rem"}}>
+					<div style={{width:"50%"}}>
+						<h1 id="heading" style={{fontSize:"30px", fontWeight:"600", width:"fit-content", paddingBottom:"20px", color:"#EF9B9A"}}>About the Blog</h1>
+						<p>This blog is about the stages of his life and the significant milestones that he has reached through his career, basically, an ultimate guide of his life. For the Harry fans that pretty much know everything there is to him, I'll also be making posts voicing my opinion on specific events or things related to him, so if you want to read more of those juicy details, head over to the Blog page. This blog has been something I had thought about creating for a long time ago when it seemed popular, but now that I was allowed to make a blog about anything, it seemed like the perfect time to finally start a blog like this. So enjoy explorin' and learnin'!</p>
+					</div>
+					<Image src="/cocoa.png" width={200} height={100} style={{width:"275px", height:"275px", borderRadius:"50%"}}/>
+				</div>
+
+				<div style={{padding:"70px 0"}}>
+					<h1 style={{fontSize:"30px", fontWeight:"600" ,borderBottom:"2px solid #C7B3B3"}}>Recent Posts</h1>
+				</div>
+
+			</div>
+			
     </div>
   );
 }
